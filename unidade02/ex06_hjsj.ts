007b6e2bb2a0c667c02a635e52f0291ff98e18d4
heclair <heclair@hotmail.com>
2023-08-29 20:48:36 -0300

    liste

---
+++ unidade02/ex06_hjsj.ts
@@ -0,0 +1,63 @@
+//2.6. Utilizando linguagem de programação defina:
+
+//a) Uma enumeração para os meses do ano.
+
+//explicação, no caso do JSON abaixo a função enum do Ts insere o número correspondente do primeiro termo evitando o índice zero do array
+
+enum MesesDoAno {
+    janeiro=1,
+    fevereiro,
+    marco,
+    abril,
+    maio,
+    junho,
+    julho,
+    agosto,
+    setembro,
+    outubro,
+    novembro,
+    dezembro,
+}
+
+console.log("Mês do Ano", MesesDoAno.novembro);  
+
+//b) Uma enumeração para os dias da semana.
+enum diasDaSemana_hjsh {
+    domingo=1,
+    segunda,
+    terca,
+    quarta,
+    quinta,
+    sexta,
+    sabado,
+}
+
+console.log("Dia da Semana", diasDaSemana_hjsh.quarta);  
+
+
+//c) Uma função recursiva para o cálculo do fatorial de um número. 
+
+function fatorial_hjsh(numero_hjsh: number): number {
+    if (numero_hjsh === 0 || numero_hjsh === 1) {
+        return 1;
+    } else {
+        return numero_hjsh * fatorial_hjsh(numero_hjsh - 1);
+    }
+}
+
+const varFat = 5;
+const resultado = fatorial_hjsh(varFat);
+console.log(`O resultado de ${varFat} fatorial é ${resultado}`);
+
+
+//d) Uma definição que corresponda a definição do tipo gênero-diferença para um uma pessoa que estude em uma faculdade. Utilize uma linguagem que dê suporte a herança. 
+
+class Genero_hjsj{
+    homem:string;
+    mulher:string;
+
+    constructor (homem:string, mulher:string){
+        this.homem = homem;
+        this.mulher = mulher;
+    }
+}
